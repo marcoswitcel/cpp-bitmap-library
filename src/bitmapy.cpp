584c1bf750884309135e8fb0125938bb59f7f84b
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-07-05 23:52:55 -0300

    mais campos

--- src/bitmapy.cpp
+++ src/bitmapy.cpp
@@ -23,10 +23,26 @@ typedef struct Bitmap_File_Header {
 } Bitmap_File_Header;
 
 /**
- * @brief DIB Header - Bitmap Informatio Header
+ * @brief DIB Header - Device-Independent Bitmap
  * @todo João, implementar
  */
 typedef struct DIB_Header {
+  /**
+   * @brief Número de bytes do header
+   */
+  uint32_t size;
+  uint32_t image_width;
+  uint32_t image_height;
+  uint16_t number_of_colors;
+  uint16_t n_bit_per_pixel;
+  uint32_t bitfield;
+  /**
+   * @brief Tamanho dos dados de imagem em bytes incluindo padding
+   */
+  uint32_t size_of_data;
+  uint32_t bitfield;
+  uint32_t print_resolution_horizontal;
+  uint32_t print_resolution_vertical;
 
 } DIB_Header;
 
