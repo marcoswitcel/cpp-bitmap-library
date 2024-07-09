f1a8ececb473924d1733c0f34b553bb71d1ce270
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-07-08 22:18:40 -0300

    loading

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,48 @@
 
 #include "./bitmap.cpp"
 
+typedef struct Byte_Array {
+  uint8_t *data;
+  size_t size;
+} Byte_Array;
+
+Byte_Array read_file_as_byte_array(const char *file_path)
+{
+  FILE *fd = fopen(file_path, "rb");
+  if (fd == NULL) {
+    fprintf(stderr, "erro abrindo o arquivo: %s\n", file_path);
+    exit(EXIT_FAILURE);
+  }
+  fseek(fd, 0, SEEK_END);
+  size_t file_size = ftell(fd);
+  fseek(fd, 0, SEEK_SET);
+
+  uint8_t *buffer = (uint8_t*) malloc(file_size * sizeof(uint8_t));
+  fread(buffer, file_size, 1, fd);
+
+  fclose(fd);
+
+  return (Byte_Array) {
+    .data = buffer,
+    .size = file_size, 
+  };
+}
+
 int main(int argc, const char* argv[])
 {
   if (argc < 1)
   {
-
     std::cout << "Nome do arquivo faltando.\n";
     return EXIT_FAILURE;
   }
 
-  const char *filename = argv[1];
+  const char *file_path = argv[1];
 
-  printf("filename: %s\n", filename);
+  printf("file path: %s\n", file_path);
+
+  auto file = read_file_as_byte_array(file_path);
+
+  printf("file size: %ld\n", file.size);
 
   return EXIT_SUCCESS;
 }
