c03a4345869a8a3e3e40ab105686a2b33f5f3c8b
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-07-21 14:39:03 -0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -33,33 +33,33 @@ void debug_print_info(Bitmap_File_Header &bmp_header, DIB_Header &dib_header, Ar
   printf("second pixel: blue: %d\n", file.data[bmp_header.offset + 5]);
 }
 
-typedef void Filter_RGB_24bits(RGB_24bits *in, RGB_24bits *out);
+typedef void Filter_RGB_24bits(const RGB_24bits *in, RGB_24bits *out);
 
-void filter_RGB_24bits_gray(RGB_24bits *in, RGB_24bits *out)
+void filter_RGB_24bits_gray(const RGB_24bits *in, RGB_24bits *out)
 {
   out->b = in->g;
   out->g = in->g;
   out->r = in->g;
 }
 
-void filter_RGB_24bits_luminosity(RGB_24bits *in, RGB_24bits *out)
+void filter_RGB_24bits_luminosity(const RGB_24bits *in, RGB_24bits *out)
 {
   out->b = in->b / 2;
   out->g = in->g / 2;
   out->r = in->r / 2;
 }
 
-void iterate_over_uncompressed_data(Bitmap_File_Header &bmp_header, DIB_Header &dib_header, Array<uint8_t> &file, Filter_RGB_24bits func)
+void iterate_over_uncompressed_data(Bitmap_File *file, Filter_RGB_24bits func)
 {
-  const unsigned row_size_in_bytes = ((dib_header.n_bit_per_pixel * dib_header.image_width + 31) / 32) * 4;
+  const unsigned row_size_in_bytes = ((file->dib->n_bit_per_pixel * file->dib->image_width + 31) / 32) * 4;
   
-  for (unsigned row = 0; row < dib_header.image_height; row++)
+  for (unsigned row = 0; row < file->dib->image_height; row++)
   {
-    const unsigned offset = bmp_header.offset + row * row_size_in_bytes;
+    const unsigned offset = file->header->offset + row * row_size_in_bytes;
 
-    for (unsigned col = 0; col < dib_header.image_width; col++)
+    for (unsigned col = 0; col < file->dib->image_width; col++)
     {
-      RGB_24bits *pixel = (RGB_24bits *) &file.data[offset + col * 3];
+      RGB_24bits *pixel = (RGB_24bits *) &file->pixel_array[offset + col * 3];
       func(pixel, pixel);
     }
   }
@@ -83,6 +83,15 @@ int main(int argc, const char* argv[])
 
   Bitmap_File_Header bmp_header = extract_bitmap_file_header_from_byte_array(file);
   DIB_Header dib_header = extract_dib_file_header_from_byte_array(file);
+  Array<uint8_t> file_pixel_array = {
+    .length = dib_header.size_of_data,
+    .data = &file[bmp_header.offset],
+  };
+  Bitmap_File bitmap_file = {
+    .header = &bmp_header,
+    .dib = &dib_header,
+    .pixel_array = &file_pixel_array,
+  };
 
   /**
    * @note Os asserts abaixo refletem alguma conclusões que tirei sobre o layout da informação,
@@ -99,7 +108,7 @@ int main(int argc, const char* argv[])
   // primeira pixel no primeiro row, segundo pixel começa no primeiro row e termina ocupando metade do segundo
   // row, aí entrariam 2 bytes de padding
   assert(dib_header.size_of_data % 4 == 0);
-  iterate_over_uncompressed_data(bmp_header, dib_header, file, filter_RGB_24bits_luminosity);
+  iterate_over_uncompressed_data(&bitmap_file, filter_RGB_24bits_luminosity);
 
 
   FILE *out = fopen("../image/image-out.bmp", "wb");
