0c89937b0922ee5a29c9803372364c9044534053
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-07-28 21:34:25 -0300

    .

--- src/main.cpp
+++ src/main.cpp
@@ -208,6 +208,30 @@ void export_generated_image()
   export_bitmap_file_to_file(&new_file, filename.c_str());
 }
 
+Bitmap_File* make_bitmap_out_of_file(Array<uint8_t> &file)
+{
+  Bitmap_File_Header *header = new Bitmap_File_Header;
+  *header = extract_bitmap_file_header_from_byte_array(file);
+
+  DIB_Header *dib = new DIB_Header;
+  *dib = extract_dib_file_header_from_byte_array(file);
+  
+  Array<uint8_t> *file_pixel_array = new Array<uint8_t>;
+  file_pixel_array->length = dib->size_of_data;
+  // @todo João, fazer uma cópia
+  file_pixel_array->data = &file[header->offset];
+
+  assert(&file.data[header->offset] == file_pixel_array->data);
+  assert(file_pixel_array->length == (file.length - header->offset));
+  
+  Bitmap_File *bitmap_file = new Bitmap_File;
+  bitmap_file->header = header;
+  bitmap_file->dib = dib;
+  bitmap_file->pixel_array = file_pixel_array;
+
+  return bitmap_file;
+}
+
 void load_and_mofidy(const char *file_path, const char *file_out_path, bool emmit_header_info)
 {
   if (emmit_header_info) printf("file path: %s\n", file_path);
@@ -216,34 +240,23 @@ void load_and_mofidy(const char *file_path, const char *file_out_path, bool emmi
 
   if (emmit_header_info) printf("file size: %ld\n", file.length);
 
-  Bitmap_File_Header bmp_header = extract_bitmap_file_header_from_byte_array(file);
-  DIB_Header dib_header = extract_dib_file_header_from_byte_array(file);
-  Array<uint8_t> file_pixel_array = {
-    .length = dib_header.size_of_data,
-    .data = &file[bmp_header.offset],
-  };
-  assert(&file.data[bmp_header.offset] == file_pixel_array.data);
-  assert(file_pixel_array.length == (file.length - bmp_header.offset));
-  Bitmap_File bitmap_file = {
-    .header = &bmp_header,
-    .dib = &dib_header,
-    .pixel_array = &file_pixel_array,
-  };
+ 
+  Bitmap_File &bitmap_file = *make_bitmap_out_of_file(file);
 
   /**
    * @note Os asserts abaixo refletem alguma conclusões que tirei sobre o layout da informação,
    * conforme for obtendo mais arquivos ".bmp" para analisar, posso validar se as assertivas são
    * verdadeiras em todas as configurações possíveis dos arquivos ".bmp"
    */
-  assert((BITMAP_FILE_HEADER_SIZE + dib_header.size) == bmp_header.offset);
-  assert((BITMAP_FILE_HEADER_SIZE + dib_header.size + dib_header.size_of_data) == bmp_header.size);
+  assert((BITMAP_FILE_HEADER_SIZE + bitmap_file.dib->size) == bitmap_file.header->offset);
+  assert((BITMAP_FILE_HEADER_SIZE + bitmap_file.dib->size + bitmap_file.dib->size_of_data) == bitmap_file.header->size);
 
-  if (emmit_header_info) debug_print_info(bmp_header, dib_header, file);
+  if (emmit_header_info) debug_print_info(*bitmap_file.header, *bitmap_file.dib, file);
 
   // @note teoricamente seriam 6 bytes úteis e 2 de padding, rows de 4 bytes
   // primeira pixel no primeiro row, segundo pixel começa no primeiro row e termina ocupando metade do segundo
   // row, aí entrariam 2 bytes de padding
-  assert(dib_header.size_of_data % 4 == 0);
+  assert(bitmap_file.dib->size_of_data % 4 == 0);
   iterate_over_uncompressed_data(&bitmap_file, filter_RGB_24bits_luminosity);
 
 
@@ -316,21 +329,11 @@ void load_and_size_down(const char *file_path, const char *file_out_path)
 {
   auto file = read_file_as_byte_array(file_path);
 
-  Bitmap_File_Header bmp_header = extract_bitmap_file_header_from_byte_array(file);
-  DIB_Header dib_header = extract_dib_file_header_from_byte_array(file);
-  Array<uint8_t> file_pixel_array = {
-    .length = dib_header.size_of_data,
-    .data = &file[bmp_header.offset],
-  };
-  assert(&file.data[bmp_header.offset] == file_pixel_array.data);
-  assert(file_pixel_array.length == (file.length - bmp_header.offset));
-  Bitmap_File bitmap_file = {
-    .header = &bmp_header,
-    .dib = &dib_header,
-    .pixel_array = &file_pixel_array,
-  };
+  Bitmap_File &bitmap_file = *make_bitmap_out_of_file(file);
 
   Image<RGB_24bits> original_picture = make_image_data_from_bitmap(bitmap_file);
+
+  auto &dib_header = *bitmap_file.dib;
 
   auto resized_down = resize_image(dib_header.image_width / 2, dib_header.image_height / 2, original_picture);
   auto resized_up = resize_image(dib_header.image_width * 2, dib_header.image_height * 2, original_picture);
