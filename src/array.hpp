db29b3aed4e8030615bdfdd018718e0671dc9f2d
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-08-06 22:04:55 -0300

    doc

--- src/array.hpp
+++ src/array.hpp
@@ -6,11 +6,18 @@
 /**
  * @brief Estrutura que representa um ponteiro para um bloco de memória com número de elementos conhecido
  * 
- * @tparam Type 
+ * @tparam Type Tipo de elementos armazenados
  */
 template <typename Type>
 struct Array {
+  /**
+   * @brief quantidade de elementos contidos no buffer
+   */
   size_t length;
+  /**
+   * @brief Ponteiro para o bloco de memória contendo os elementos
+   * 
+   */
   Type *data;
 
   Type& operator[](size_t index)
--- src/color-sampler.hpp
+++ src/color-sampler.hpp
@@ -2,9 +2,26 @@
 
 #include "./array.hpp"
 
+/**
+ * @brief Define os metadados necessários para interpretar o buffer de cores que contém a informação
+ * para produzir a imagem final.
+ * 
+ * @tparam Pixel_Type O tipo de cor escolhido para trabalhar, aqui podem haver diversas variações desde precisão
+ * a quantidade de canais e se o range é dinâmico ou fixo.
+ */
 template <typename Pixel_Type>
 struct Image {
+  /**
+   * @brief largura da imagem
+   */
   size_t width;
+  /**
+   * @brief altura da imagem
+   */
   size_t height;
+  /**
+   * @brief Buffer com as informações necessárias para produzir a image, o Pixel_Type pode assumir um formato
+   * "incompleto", por exemplo, quando os dados representam index em uma paleta de cores.
+   */
   Array<Pixel_Type> *buffer;
 };
