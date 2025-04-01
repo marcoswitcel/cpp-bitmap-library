ecf7d94a2c98295c5a1d4f01736e8d22495e45c5
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-03-31 21:44:23 -0300

    refatorando

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,11 @@
 #include <math.h>
 
 #include "./bitmap.cpp"
+#include "./debug.cpp"
 #include "./array.hpp"
 #include "./filters.cpp"
 #include "./command-line-utils.cpp"
 #include "./color-sampler.cpp"
-
-
-void debug_print_info(Bitmap_File_Header &bmp_header, DIB_Header &dib_header, Array<uint8_t> &file)
-{
-  printf("%c %c\n", bmp_header.header[0], bmp_header.header[1]);
-  printf("header: file size: %d\n", bmp_header.size);
-  printf("header: file offset: %d\n", bmp_header.offset);
-  printf("dib: file size: %d\n", dib_header.size);
-  printf("dib: width: %d\n", dib_header.image_width);
-  printf("dib: height: %d\n", dib_header.image_height);
-
-  printf("dib: number_of_colors: %d\n", dib_header.number_of_colors_planes);
-  printf("dib: n_bit_per_pixel: %d\n", dib_header.n_bit_per_pixel);
-  printf("dib: bitfield: %d\n", dib_header.bitfield);
-  printf("dib: size_of_data: %d\n", dib_header.size_of_data);
-  printf("dib: print_resolution_horizontal: %d\n", dib_header.print_resolution_horizontal);
-  printf("dib: print_resolution_vertical: %d\n", dib_header.print_resolution_vertical);
-  printf("dib: n_colors_in_palette: %d\n", dib_header.n_colors_in_palette);
-  printf("dib: important_colors: %d\n", dib_header.important_colors);
-
-  printf("file: %ld bytes\n", file.length);
-}
 
 
 /**
